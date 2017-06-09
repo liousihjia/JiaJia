b5e065fe1fc0b2c53eb10174638789e60f238952
JiaJia <liousihjia@gmail.com>
2017-06-09 17:55:57 +0800

    2017-06-09

--- JiaJiaDiary/src/main/java/com/programcreek/helloworld/controller/testJDBCController.java
+++ JiaJiaDiary/src/main/java/com/programcreek/helloworld/controller/testJDBCController.java
@@ -1,8 +1,5 @@
 package com.programcreek.helloworld.controller;
  
-import javax.validation.Valid;
-
-import org.springframework.samples.mvc.messageconverters.JavaBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
