7b92b6df6527dd40fbf993ca501d5756faaad753
JiaJia <liousihjia@gmail.com>
2017-06-16 16:10:05 +0800

    2017-06-16 jsl測試 DB
    Change-Id: I8a1b6a9d831340ac5c7804e33dc02a63b7870a81
    Signed-off-by: JiaJia <liousihjia@gmail.com>

---
+++ jxlSimple/src/main/java/test/Employee.java
@@ -0,0 +1,56 @@
+package test;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Employee {
+	private String name;
+    private Date birthDate;
+    private int payment;
+    private double bonus;
+    
+    public Employee() {
+	}
+    
+	
+	public Employee(String string, Date parse, int i, double d) {
+		this.name = string;
+		this.birthDate = parse;
+		this.payment = i;
+		this.bonus = d;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+
+	public int getPayment() {
+		return payment;
+	}
+
+
+	public void setPayment(int payment) {
+		this.payment = payment;
+	}
+
+
+	public double getBonus() {
+		return bonus;
+	}
+
+
+	public void setBonus(double bonus) {
+		this.bonus = bonus;
+	}
+	
+}
